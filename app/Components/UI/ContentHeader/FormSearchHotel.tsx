cde322a30c41459f50b95f98e99fa5d12ba4167c
lyhao223 <lyhao0710@gmail.com>
2024-08-16 14:20:31 +0700

    fix icon search

--- app/Components/UI/ContentHeader/FormSearchHotel.tsx
+++ app/Components/UI/ContentHeader/FormSearchHotel.tsx
@@ -119,8 +119,8 @@ const FormSearchHotel = () => {
     <>
       <form
         onSubmit={handleSubmit}
-        className="flex 2xl:flex-row 2xl:items-start 2xl:justify-start 2xl:space-x-8 2xl:space-y-0 xl:flex-row xl:items-start xl:justify-start xl:space-x-8 xl:space-y-0 lg:flex-row lg:items-start lg:justify-start lg:space-x-8 lg:space-y-0 md:flex-col md:items-start md:justify-start md:space-y-8 ls:flex-col ls:items-start ls:justify-start ls:space-x-0 ls:space-y-4 ms:flex-col ms:items-start ms:justify-start ms:space-x-0 ms:space-y-4 xs:flex-col xs:items-start xs:justify-start xs:space-x-0 xs:space-y-4">
-        <div className="flex flex-row items-start justify-start space-x-1">
+        className="relative flex 2xl:flex-row 2xl:items-start 2xl:justify-start 2xl:space-x-8 2xl:space-y-0 xl:flex-row xl:items-start xl:justify-start xl:space-x-8 xl:space-y-0 lg:flex-row lg:items-start lg:justify-start lg:space-x-8 lg:space-y-0 md:flex-col md:items-start md:justify-start md:space-y-8 ls:flex-col ls:items-start ls:justify-start ls:space-x-0 ls:space-y-4 ms:flex-col ms:items-start ms:justify-start ms:space-x-0 ms:space-y-4 xs:flex-col xs:items-start xs:justify-start xs:space-x-0 xs:space-y-4">
+        <div className="relative flex flex-row items-start justify-start space-x-1">
           <span>
             <CiLocationOn className="2xl:w-14 2xl:h-14 xl:w-14 xl:h-14 lg:w-14 lg:h-14 md:w-14 md:h-14 ls:w-14 ls:h-14 ms:w-14 ms:h-14 xs:h-10 xs:w-10" />
           </span>
@@ -136,7 +136,7 @@ const FormSearchHotel = () => {
             className="2xl:w-full xl:w-full lg:w-32 md:w-[34.222rem] ls:w-full"
           />
         </div>
-        <div className="flex flex-row items-start justify-start space-x-1">
+        <div className="relative flex flex-row items-start justify-start space-x-1">
           <span>
             <CiCalendar className="2xl:w-14 2xl:h-14 xl:w-14 xl:h-14 lg:w-14 lg:h-14 md:w-14 md:h-14 ls:w-14 ls:h-14 ms:w-14 ms:h-14 xs:h-10 xs:w-10" />
           </span>
@@ -173,7 +173,7 @@ const FormSearchHotel = () => {
           </div>
         </div>
 
-        <div className="flex flex-row items-start justify-start space-x-1">
+        <div className="relative flex flex-row items-start justify-start space-x-1">
           <span>
             <IoMdPeople className="2xl:w-14 2xl:h-14 xl:w-14 xl:h-14 lg:w-14 lg:h-14 md:w-14 md:h-14 ls:w-14 ls:h-14 ms:w-14 ms:h-14 xs:h-10 xs:w-10" />
           </span>
@@ -231,10 +231,8 @@ const FormSearchHotel = () => {
             </Select>
           </FormControl>
         </div>
-        <button
-          type="submit"
-          className="absolute 2xl:right-16 2xl:top-20 xl:right-10 xl:top-20 lg:-right-4 lg:top-20 md:-right-8 md:-top-5 ls:-right-5 ls:top-0 ms:-right-3 ms:top-0 xs:-right-4 xs:-top-3">
-          <div className="relative rounded-full border-2 border-slate-100 group bg-purple-400 p-2 hover:bg-purple-800 hover:duration-200 hover:transition-all hover:ease-in-out">
+        <button type="submit" className="relative">
+          <div className="absolute 2xl:top-0 2xl:left-16 xl:top-0 xl:left-0 lg:top-0 lg:-left-8 md:-top-80 md:left-[35rem] ls:-top-[21rem] ls:left-72 ms:-top-[21rem] ms:left-64 xs:left-52 xs:-top-[21rem] rounded-full border-2 border-slate-100 group bg-purple-400 p-2 hover:bg-purple-800 hover:duration-200 hover:transition-all hover:ease-in-out">
             <IoIosSearch className="w-8 h-8 group-hover:text-slate-100" />
           </div>
         </button>
